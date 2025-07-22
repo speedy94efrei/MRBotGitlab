6bd1278e3f3b3bbcfbc73ff1b42f4ce9f9be7e04
Sullivan ALLAMELOU <sallamelou@dailybiz.com>
2025-07-22 17:20:38 +0200

    GRRRRRR A

--- main.py
+++ main.py
@@ -6,16 +6,31 @@ from dotenv import load_dotenv
 app = Flask(__name__)
 load_dotenv() # charge variables d'environnement
 
+Lions = {
+    "Rayan", "Sullivan", "Steven", "Aymen", "Moustapha"
+}
+Titans = {
+    "Aymen", "Youssef", "Walid", "Jawad", "Andy"
+}
+Chevaliers = {
+    "Florian", "Nassim", "Moustoifa", "Nicolas"
+}
+Gardiens = {
+    "Faïz", "Massil", "Kristina", "Sara"
+}
+
 # === Fonction de résolution du webhook cible ===
 def get_webhook_url(canalCible: int) -> str:
-    if canalCible == 1:
-        return os.getenv("TEAMS_WEBHOOK_CANAL1")
-    elif canalCible == 2:
-        return os.getenv("TEAMS_WEBHOOK_CANAL2")
-    elif canalCible == 3:
-        return os.getenv("TEAMS_WEBHOOK_CANAL3")
+    if canalCible == "Lions":
+        return os.getenv("TEAMS_WEBHOOK_Lions")
+    elif canalCible == "Titans":
+        return os.getenv("TEAMS_WEBHOOK_Titans")
+    elif canalCible == "Chevaliers":
+        return os.getenv("TEAMS_WEBHOOK_Chevaliers")
+    elif canalCible == "Gardiens":
+        return os.getenv("TEAMS_WEBHOOK_Gardiens")
     else:
-        return os.getenv("TEAMS_WEBHOOK_CANAL4")
+        return 0
 
 # === Route de réception GitLab ===
 @app.route("/gitlab-webhook", methods=["POST"])
@@ -37,11 +52,10 @@ def gitlab_webhook():
                     f"\n**Auteur** : {author}\n"
                     f"\n**Reviewers** :\n{reviewer_list}\n"
                     f"\n[\ud83d\udcc8 Voir la MR]({url})"
+                    f"{data}"
         }
 
-        canalCible = 0
-        if author == "Massil":
-            canalCible = 1
+        canalCible = "Lions"
 
         webhook_url = get_webhook_url(canalCible)
         response = requests.post(webhook_url, json=message) 
